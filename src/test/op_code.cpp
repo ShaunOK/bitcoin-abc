e843ad99153df47173120d4b282a00a6fd0eebd9
Marcos Mayorga <mm@mm-studios.com>
2018-02-13 12:28:26 +0000

    CAT+SPLIT tests

--- src/test/op_code.cpp
+++ src/test/op_code.cpp
@@ -177,30 +177,120 @@ cout << "neg: " << neg << endl;
 		CScript script;
 		script << OP_CAT;
 
+		//two inputs required
 		test(script,stack_t(),flags,SCRIPT_ERR_INVALID_STACK_OPERATION);
 		test(script,stack_t{{0x00}},flags,SCRIPT_ERR_INVALID_STACK_OPERATION);
+
+		//stack item with maximum length		
+		item maxlength_item(MAX_SCRIPT_ELEMENT_SIZE, 0x00);
+
+		//Concatenation producing illegal sized output
+		{
+		stack_t input_stack;
+		input_stack.push_back(maxlength_item);
+		item i;
+		i.push_back(0x00);
+		input_stack.push_back(i);
+		test(script,input_stack,flags,SCRIPT_ERR_PUSH_SIZE);
+		}
+
+	
+		//Concatenation of a max-sized item with empty is legal
+		{
+		stack_t input_stack;
+		input_stack.push_back(maxlength_item);
+		input_stack.push_back(item()); //empty item
+		test(script,input_stack,flags,stack_t{maxlength_item});
+		}
+		{
+		stack_t input_stack;
+		input_stack.push_back(item()); //empty item
+		input_stack.push_back(maxlength_item);
+		test(script,input_stack,flags,stack_t{maxlength_item});
+		}
+
+		//Concatenation of a zero length operand
+		test(script,stack_t{{0x01},{}},flags,stack_t{{0x01}});
+		test(script,stack_t{{},{0x01}},flags,stack_t{{0x01}});
+
+		//Concatenation of two empty operands results in empty item
+		test(script,stack_t{{},{}},flags,stack_t{{}});
+
+		// concatenating two operands generates the correct result
 		test(script,stack_t{{0x00},{0x00}},flags,stack_t{{0x00,0x00}});
 		test(script,stack_t{{0x01},{0x02}},flags,stack_t{{0x01,0x02}});
+		test(script,stack_t{{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a},{0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14}},flags,stack_t{{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14}});
+
 
 	}
 
 	void test_split(uint32_t flags) {
 		CScript script;
-		script << OP_SPLIT;
+		script << OP_SPLIT; //inputs: x n; outputs: x1 x2
 
+		//two inputs required
 		test(script,stack_t(),flags,SCRIPT_ERR_INVALID_STACK_OPERATION);
 		test(script,stack_t{{0x01}},flags,SCRIPT_ERR_INVALID_STACK_OPERATION);
+
+		//length of 2nd input greater than CScriptNum::nDefaultMaxNumSize
+		{
+		item maxlength_num_item(CScriptNum::nDefaultMaxNumSize,0x01);
+		item illegal_item=maxlength_num_item;
+		illegal_item.push_back(0x00);
+		test(script,stack_t{{0x01},illegal_item},flags,SCRIPT_ERR_UNKNOWN_ERROR);
+		}
+
+		//if n == 0, then x1 is the empty array and x2 == x;
+		//execution of OP_SPLIT on empty array results in two empty arrays.
 		test(script,stack_t{{},{}},flags,stack_t{{},{}});
-		test(script,stack_t{{0x01},{}},flags,stack_t{{},{0x01}});
+		test(script,stack_t{{0x01},{}},flags,stack_t{{},{0x01}}); //x 0 OP_SPLIT -> OP_0 x
 		test(script,stack_t{{0x01,0x02,0x03,0x04},{}},flags,stack_t{{},{0x01,0x02,0x03,0x04}});
 
-		test(script,stack_t{{0x01},{0x01}},flags,stack_t{{0x01},{}});
+		//if n == len(x) then x1 == x and x2 is the empty array
+		test(script,stack_t{{0x01},{0x01}},flags,stack_t{{0x01},{}}); 
+		test(script,stack_t{{0x01,0x02,0x03},{0x03}},flags,stack_t{{0x01,0x02,0x03},{}}); //x len(x) OP_SPLIT -> x OP_0
+
+		//if n > len(x), then the operator must fail; x (len(x) + 1) OP_SPLIT -> FAIL
+		test(script,stack_t{{},{0x01}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
+		test(script,stack_t{{0x01},{0x02}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
+		test(script,stack_t{{0x01,0x02,0x03},{0x04}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
+		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x05}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
+
+		//if n < 0 the operator must fail.
+		test(script,stack_t{{0x01,0x02,0x03,0x04},{0xff,0xff,0xff,0xff}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
+
+
 		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x01}},flags,stack_t{{0x01},{0x02,0x03,0x04}});
 		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x02}},flags,stack_t{{0x01,0x02},{0x03,0x04}});
 		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x03}},flags,stack_t{{0x01,0x02,0x03},{0x04}});
 		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x04}},flags,stack_t{{0x01,0x02,0x03,0x04},{}});
-		test(script,stack_t{{0x01,0x02,0x03,0x04},{0x05}},flags,SCRIPT_ERR_INVALID_SPLIT_RANGE);
 
+		//split of a max-len item
+		{
+		item maxlength_item(MAX_SCRIPT_ELEMENT_SIZE, 0x00);
+		test(script,stack_t{maxlength_item,{0x00}},flags,stack_t{{},maxlength_item});
+		item illegal_item(MAX_SCRIPT_ELEMENT_SIZE+1, 0x00);
+		test(script,stack_t{maxlength_item,{0x00}},flags,SCRIPT_ERR_PUSH_SIZE);
+		}
+		
+
+
+	}
+
+	void test_cat_split(const item& x, uint32_t flags) {
+		CScript script;
+		script << OP_SPLIT << OP_CAT;
+		// x n OP_SPLIT OP_CAT -> x - for all x and for all 0 <= n <= len(x)
+		for (uint8_t i=0; i<=x.size(); ++i) {
+			test(script,stack_t{x,{i}},flags,stack_t{x});
+		}
+	}
+
+	void test_cat_split(uint32_t flags) {
+		test_cat_split({},flags);
+		test_cat_split({0x01},flags);
+		test_cat_split({0x01,0x02},flags);
+		test_cat_split({0x01,0x02,0x03},flags);
 	}
 
 	void test_and(uint32_t flags) {
@@ -282,6 +372,14 @@ BOOST_AUTO_TEST_CASE(op_split) {
 	test_split(STANDARD_NOT_MANDATORY_VERIFY_FLAGS);
 	test_split(STANDARD_LOCKTIME_VERIFY_FLAGS);
 }
+
+BOOST_AUTO_TEST_CASE(cat_split) {
+	test_cat_split(0);
+	test_cat_split(STANDARD_SCRIPT_VERIFY_FLAGS);
+	test_cat_split(STANDARD_NOT_MANDATORY_VERIFY_FLAGS);
+	test_cat_split(STANDARD_LOCKTIME_VERIFY_FLAGS);
+}
+/*
 BOOST_AUTO_TEST_CASE(op_and) {
 	test_and(0);
 	test_and(STANDARD_SCRIPT_VERIFY_FLAGS);
@@ -325,7 +423,7 @@ BOOST_AUTO_TEST_CASE(op_bin2num) {
 	test_bin2num(STANDARD_NOT_MANDATORY_VERIFY_FLAGS);
 	test_bin2num(STANDARD_LOCKTIME_VERIFY_FLAGS);
 }
-
+*/
 
 BOOST_AUTO_TEST_SUITE_END()
 
